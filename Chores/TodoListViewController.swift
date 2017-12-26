9c2d12db1e58fddb74a4b11d01b05545a4e8dbb5
William Proudfoot <bill.proudfoot@btinternet.com>
2017-12-26 14:36:37 +0000

    Tableview set up

--- Chores/TodoListViewController.swift
+++ Chores/TodoListViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let itemArray = ["buy milk", "paint door", "make dinner"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
 }
 
